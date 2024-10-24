74efd6bd3bd881d02cedaefe5632be0054608dda
habib <157394402+habibbedawi@users.noreply.github.com>
2024-10-24 23:55:06 +0300

    main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function headerShadow() {
 }
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["CEO","CEO"],
+  strings : ["CEO","Bazan Institute"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
